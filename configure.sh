23c9ebc327f17ad77717b733a3ba6751d1fe97ba
eschnett <eschnett@4054007a-4839-4648-a056-eb81e030d849>
2015-08-13 15:58:25 +0000

    Support Cygwin
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/OpenCL/trunk@43 4054007a-4839-4648-a056-eb81e030d849

--- configure.sh
+++ configure.sh
@@ -25,7 +25,7 @@ if [ -z "${OPENCL_DIR}" ]; then
     # look into each directory
     for dir in $DIRS; do
         # libraries might have different file extensions
-        for libext in a so dylib; do
+        for libext in a dll dll.a dylib so; do
             # libraries can be in /lib or /lib64
             for libdir in lib64 lib; do
                 FILES="$libdir/libOpenCL.${libext}"
